249d86262f31a30ccf4e997dea29b7cd3d6a1b78
Manjinder Singh <hundalrajan6@gmail.com>
2021-05-30 11:06:57 +1200

    Add project files.

---
+++ RaceDay/Danish.cs
@@ -0,0 +1,12 @@
+﻿namespace RaceDay
+{
+    class Danish : Punter // Danish class inherting the feature of Punter class
+    {
+        public Danish() // Danish Constructor
+        {
+            PunterName = "Danish"; // Punter name
+            CashOnHand = 50; // Initial amount for punter
+            Alias = "Danish"; // Punter Nick Name
+        }
+    }
+}
